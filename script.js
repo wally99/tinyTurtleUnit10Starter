6e312ca505b94bc60390a880a2f9942f571eb167
Wilder <wilderbravo2000@gmail.com>
2017-03-22 12:32:48 +0000

    Shape

--- script.js
+++ script.js
@@ -1,18 +1,56 @@
 TinyTurtle.apply(window, [undefined, 400, 400]);
 
 function square(){
-forward(50);
+forward(40);
 right(90);
-forward(50);
+forward(40);
 right(90);
-forward(50);
+forward(40);
 right(90);
-forward(50);
+forward(40);
 right(90);
-forward(50);
+forward(40);
 }
 
-// Type your function call below
+function triangle(x){
+right(30);
+forward(x);
+right(120);
+forward(x);
+right(120);
+forward(x);
+}
+
+function rectangle(x,y){
+forward(x);
+right(90);
+forward(y);
+right(90);
+forward(x);
+right(90);
+forward(y);
+
+}
+//triangle(70);
+//square(70);
+rectangle(40,100);
+
+function pentagon(size){
+right(30);
+forward(size);
+right(120);
+forward(size);
+right(30);
+forward(size);
+right(90)
+forward(size);
+right(90);
+forward(size);
+
+}
+//pentagon(50);
 
 
-stamp();
+
+
+
